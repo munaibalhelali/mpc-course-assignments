2877d1fe7ef2134f84f11ccfcb32d95fe2f7329b
munaibalhelali <moneebalhelaly@yahoo.com>
2022-03-17 22:09:04 +0100

    Solved assignment 2 using RMS

--- assignment2.py
+++ assignment2.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sim.sim2d import sim_run
-
 # Simulator options.
 options = {}
 options['FIG_SIZE'] = [8,8]
@@ -8,12 +7,12 @@ options['OBSTACLES'] = False
 
 class ModelPredictiveControl:
     def __init__(self):
-        self.horizon = 2
+        self.horizon = 20
         self.dt = 0.2
 
         # Reference or set point the controller will achieve.
-        self.reference1 = [10, 10, 0]
-        self.reference2 = None #[10, 2, 3.14/2]
+        self.reference1 = [3, 10, 0]
+        self.reference2 = [10, 2, 3*3.14/2]
 
     def plant_model(self,prev_state, dt, pedal, steering):
         x_t = prev_state[0]
@@ -21,12 +20,32 @@ class ModelPredictiveControl:
         psi_t = prev_state[2]
         v_t = prev_state[3]
 
+        v_t = v_t + dt * pedal - v_t/25.0
+        
+        x_t = x_t + dt * v_t * np.cos(psi_t)
+        y_t = y_t + dt * v_t * np.sin(psi_t) 
+        psi_t += dt * v_t *np.tan(steering)/2.5
+        
+        
         return [x_t, y_t, psi_t, v_t]
 
     def cost_function(self,u, *args):
         state = args[0]
         ref = args[1]
         cost = 0.0
+        
+        for k in range(self.horizon):
+            v_start = state[3]
+            state = self.plant_model(state, self.dt, u[k*2], u[k*2+1])
+            x_diff = abs(state[0] - ref[0])
+            y_diff = abs(state[1] - ref[1])
+            psi_diff = abs(state[2] - ref[2])
+            cost += np.sqrt(x_diff**2+y_diff**2 + psi_diff**2 + (state[3] - v_start)**2)
+
+        speed_kph = state[3]*3.6
+        if speed_kph > 10.0:
+            cost += speed_kph * 100
+
         return cost
 
 sim_run(options, ModelPredictiveControl)
