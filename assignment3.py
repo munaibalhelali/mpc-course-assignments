3c88a774db89f920fb2c84d9d63fc2e71fc58e7a
munaibalhelali <moneebalhelaly@yahoo.com>
2022-03-17 22:39:10 +0100

    Solved assignment 3 with obstical avoidance

--- assignment3.py
+++ assignment3.py
@@ -8,11 +8,11 @@ options['OBSTACLES'] = True
 
 class ModelPredictiveControl:
     def __init__(self):
-        self.horizon = 2
+        self.horizon = 20
         self.dt = 0.2
 
         # Reference or set point the controller will achieve.
-        self.reference1 = [10, 0, 0]
+        self.reference1 = [10, 0, 45*3.14/180]
         self.reference2 = None
 
         self.x_obs = 5
@@ -24,12 +24,41 @@ class ModelPredictiveControl:
         psi_t = prev_state[2]
         v_t = prev_state[3]
 
+        v_t = v_t + dt * pedal - v_t/25.0
+        
+        x_t = x_t + dt * v_t * np.cos(psi_t)
+        y_t = y_t + dt * v_t * np.sin(psi_t) 
+        psi_t += dt * v_t *np.tan(steering)/2.5
+        
+        
         return [x_t, y_t, psi_t, v_t]
 
     def cost_function(self,u, *args):
         state = args[0]
         ref = args[1]
         cost = 0.0
+        
+        for k in range(self.horizon):
+            v_start = state[3]
+            state = self.plant_model(state, self.dt, u[k*2], u[k*2+1])
+            x_diff = abs(state[0] - ref[0])
+            y_diff = abs(state[1] - ref[1])
+            psi_diff = abs(state[2] - ref[2])
+
+            obs_dist_x = abs(state[0] - self.x_obs)
+            obs_dist_y = abs(state[1] - self.y_obs)
+
+            obs_dist = np.sqrt(obs_dist_x**2 + obs_dist_y**2)
+
+
+            cost += np.sqrt(x_diff**2+y_diff**2 + psi_diff**2 + (state[3] - v_start)**2)
+            
+            cost += 1/obs_dist**2*10
+
+        speed_kph = state[3]*3.6
+        if speed_kph > 10.0:
+            cost += speed_kph * 100
+
         return cost
 
 sim_run(options, ModelPredictiveControl)
